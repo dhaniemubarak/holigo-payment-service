8b2f261f5445d96078be58508be9bdad938b20fd
dhaniemubarak <dhaniemubarak@gmail.com>
2022-01-14 18:56:18 +0700

    Added: Transaction DTO

---
+++ src/main/java/id/holigo/services/common/model/TransactionDto.java
@@ -0,0 +1,61 @@
+package id.holigo.services.common.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TransactionDto {
+    
+    private UUID id;
+
+    private UUID parentId;
+
+    private String shortId;
+
+    private Long userId;
+
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
+
+    private Timestamp deletedAt;
+
+    private Timestamp expiredAt;
+
+    private BigDecimal discountAmount;
+
+    private BigDecimal fareAmount;
+
+    private BigDecimal ntaAmount;
+
+    private BigDecimal nraAmount;
+
+    private Integer serviceId;
+
+    private Integer productId;
+
+    private String indexUser;
+
+    private String indexCommission;
+
+    private String indexProduct;
+
+    private String transactionId;
+
+    private String transactionType;
+
+    private UUID paymentId;
+
+    private StatusPaymentEnum statusPayment;
+
+    private StatusOrderEnum statusOrder;
+}
