78689c82680630e2f0e99f1fcf4e47e5761647bc
dhaniemubarak <dhaniemubarak@gmail.com>
2022-01-14 18:56:39 +0700

    Added: Jms Configuration

---
+++ src/main/java/id/holigo/services/holigopaymentservice/config/JmsConfig.java
@@ -0,0 +1,20 @@
+package id.holigo.services.holigopaymentservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+    public static final String GET_TRANSACTION_BY_ID = "get-transaction-by-id";
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
