2c08c96fa0b1dec0df2343e0fe91bb183bf40b86
dhaniemubarak <dhaniemubarak@gmail.com>
2022-01-14 18:56:56 +0700

    Added: Payment domain

---
+++ src/main/java/id/holigo/services/holigopaymentservice/domain/Payment.java
@@ -0,0 +1,99 @@
+package id.holigo.services.holigopaymentservice.domain;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import id.holigo.services.common.model.StatusPaymentEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Payment {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    private UUID id;
+
+    @Column(length = 36, columnDefinition = "varchar(36)", nullable = true)
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    private UUID transactionId;
+
+    private Long userId;
+
+    private String paymentServiceId;
+
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal fareAmount;
+
+    @Column(precision = 10, scale = 2, nullable = false, columnDefinition = "decimal(10,2) default 0")
+    private BigDecimal serviceFeeAmount;
+
+    @Column(precision = 10, scale = 2, nullable = false, columnDefinition = "decimal(10,2) default 0")
+    private BigDecimal discountAmount;
+
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal totalAmount;
+
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal paymentServiceAmount;
+
+    private Boolean isSplitBill;
+
+    @Column(precision = 10, scale = 2, nullable = false, columnDefinition = "decimal(10,2) default 0")
+    private BigDecimal pointAmount;
+
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal remainingAmount;
+
+    private StatusPaymentEnum status;
+
+    @Column(nullable = true)
+    private String verifyType;
+
+    @Column(nullable = true)
+    private String verifyId;
+
+    private String detailType;
+
+    private String detailId;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+    @Column(nullable = true)
+    private Timestamp deletedAt;
+
+    @Transient
+    private String voucherCode;
+
+    @Transient
+    private String pin;
+
+}
