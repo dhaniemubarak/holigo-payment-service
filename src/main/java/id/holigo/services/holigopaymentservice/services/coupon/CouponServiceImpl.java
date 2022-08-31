eff8cf80074b263c58669d9f320519ef5245ea3d
Mochamad Ramdhanie Mubarak <dhaniemubarak@gmail.com>
2022-09-01 02:45:55 +0700

    Get apply coupon

---
+++ src/main/java/id/holigo/services/holigopaymentservice/services/coupon/CouponServiceImpl.java
@@ -0,0 +1,23 @@
+package id.holigo.services.holigopaymentservice.services.coupon;
+
+import id.holigo.services.common.model.ApplyCouponDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CouponServiceImpl implements CouponService {
+
+    private CouponServiceFeignClient couponServiceFeignClient;
+
+    @Autowired
+    public void setCouponServiceFeignClient(CouponServiceFeignClient couponServiceFeignClient) {
+        this.couponServiceFeignClient = couponServiceFeignClient;
+    }
+
+    @Override
+    public ApplyCouponDto applyCoupon(UUID transactionId, String couponCode, String paymentServiceId, Long userId) {
+        return null;
+    }
+}
