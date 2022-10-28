cf70220f000459c1942d5a7c87068452810adb94
Mochamad Ramdhanie Mubarak <dhaniemubarak@gmail.com>
2022-10-29 02:12:50 +0700

    Add DANA payment method

--- src/main/java/id/holigo/services/holigopaymentservice/HoligoPaymentServiceApplication.java
+++ src/main/java/id/holigo/services/holigopaymentservice/HoligoPaymentServiceApplication.java
@@ -4,16 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import java.time.Year;
-import java.util.Calendar;
-import java.util.Stack;
-
 @EnableFeignClients
 @SpringBootApplication
 public class HoligoPaymentServiceApplication {
 
     public static void main(String[] args) {
-        System.out.println();
         SpringApplication.run(HoligoPaymentServiceApplication.class, args);
     }
 
